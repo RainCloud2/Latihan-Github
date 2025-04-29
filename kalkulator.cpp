fe25b428a2121432d861a48287c98a86cf83421a
RainCloud2 <rainer.siregar@yahoo.com>
2025-04-29 12:11:56 +0700

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+	
+	int num1, num2, x, hasil;
+	printf ("Type in first number : ");
+	scanf ("%d", &num1);
+	printf ("Type in second number : ");
+	scanf ("%d", &num2);
+	
+	printf ("select equation :\n");
+	printf ("1. addition (+)\n");
+	printf ("2. subtracton (-)\n");
+	printf ("3. multiplication (x)\n");
+	printf ("4. division (:)\n");
+	scanf ("%d", &x);
+	if (x=1) hasil = num1 + num2
+	printf ("result : %d", &hasil);
+	
+}
